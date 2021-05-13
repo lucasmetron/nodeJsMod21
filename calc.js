ca4d6189c67a0d7c7dc2ed210c1b8397d9fd8b4f
lucasmetron <lucasmetron@gmail.com>
2021-05-13 10:26:18 -0300

    modulo

---
+++ calc.js
@@ -0,0 +1,13 @@
+let calc = {
+    mult: function (x, y) {
+        return x * y;
+    },
+    soma: (x, y) => {
+        return x + y;
+    },
+
+    erro: "Opção inválida"
+}
+
+
+module.exports = calc;
