67ea9b6dfa895e3464d91f11fa8026a291eaab05
lucasmetron <lucasmetron@gmail.com>
2021-05-13 16:09:19 -0300

    aprendendo sobre pacotes

---
+++ operacoesBasicas.js
@@ -0,0 +1,8 @@
+
+module.exports.soma = function (a, b) { return a + b; }
+
+module.exports.sub = function (a, b) { return a - b; }
+
+module.exports.mult = function (a, b) { return a * b; }
+
+module.exports.div = function (a, b) { return a / b; }
--- script.js
+++ script.js
@@ -1,38 +1,4 @@
-let ops = require("./funcoes");
+const operacoesBasicas = require("./operacoesBasicas");
+const areas = require("./areas");
 
-let args = process.argv.slice(2);
-let fileName = args[1];
-let fileContent = args[2];
-let op = args[0]
-
-
-if (op === "criar") {
-    ops.criar(fileName, fileContent)
-}
-
-else if (op === "ler") {
-    ops.ler(fileName)
-}
-
-else if (op === "deletar") {
-    ops.delet(fileName)
-}
-
-else if (op === "alterar") {
-    ops.inserir(fileName, fileContent)
-}
-
-else if (op === "renomear") {
-    ops.renomear(fileName, fileContent)
-}
-
-else {
-    console.log("Opção inválida!")
-}
-
-
-
-
-
-
-
+module.exports = { ...operacoesBasicas, ...areas }
