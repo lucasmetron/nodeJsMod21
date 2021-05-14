3cb01b782fa79a9614d1bbbe6a85c9bbe689096b
lucasmetron <lucasmetron@gmail.com>
2021-05-14 09:37:37 -0300

    pacotes

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const operacoesBasicas = require("./operacoesBasicas");
 const areas = require("./areas");
 
+let calc = {
+    operacoesBasicas,
+    areas
+};
+
+
 module.exports = { ...operacoesBasicas, ...areas }
+
+console.log(operacoesBasicas.mult(5, 9))
