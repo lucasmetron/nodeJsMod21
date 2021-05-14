3b5f6d7d964740939bef58db9d82f5033aa4c564
lucasmetron <lucasmetron@gmail.com>
2021-05-14 10:35:21 -0300

    biblioteca sharp

--- imagem/app.js
+++ imagem/app.js
@@ -1,9 +1,23 @@
+const sharp = require('sharp');
+
 let path = process.argv[2];
 let width = Number(process.argv[3]);
 
 console.log(path, width)
 
-let resize = (path, width) => {
+function resize(path, width) {
+
+    sharp(path).resize({ width: width })
+        .toFile('./temp/output_rezise.jpg', (erro) => {
+            if (erro) {
+                console.log("Deu ruim " + erro)
+            } else {
+                console.log("imagem redimensionada com sucesso!")
+            }
+        })
+
 
 
 }
+
+resize(path, width);
