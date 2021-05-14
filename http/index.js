887e9c164af10fc77d69359bdee6f26d93349deb
lucasmetron <lucasmetron@gmail.com>
2021-05-14 14:50:55 -0300

    servidor de páginas

---
+++ http/index.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs')
+
+http.createServer((request, response) => {
+
+    let path = url.parse(request.url).pathname;
+    let fileName = "." + path;
+
+    fs.readFile(fileName, (erro, data) => {
+        if (erro) {
+            response.writeHead(404, { "Content-Type": "text/html; charset=utf8" })
+            response.end("<h1>Página não encontrada</h1>")
+        } else {
+            response.writeHead(200, { "Content-Type": "text/html" })
+            response.write(data);
+            response.end();
+        }
+
+
+    })
+
+}).listen(3000, erro => {
+    if (erro) {
+        console.log(erro)
+    } else {
+        console.log("servidor rodando na porta 3000")
+    }
+
+})
