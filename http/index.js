5f0639ed95075267b5768a5b5f57d7f9b51c3494
lucasmetron <lucasmetron@gmail.com>
2021-05-17 10:48:02 -0300

    request

--- http/index.js
+++ http/index.js
@@ -2,28 +2,48 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs')
 
-http.createServer((request, response) => {
 
-    let path = url.parse(request.url).pathname;
-    if (path == "" || path == "/") { //caso não seja digitado a página especifica, redireciona para a index
-        path = "/index.html"
-    }
+function handleFile(req, res, callback) {
+
+    let path = url.parse(req.url).pathname;
     let fileName = "." + path;
-
-
 
     fs.readFile(fileName, (erro, data) => {
         if (erro) {
-            response.writeHead(404, { "Content-Type": "text/html; charset=utf8" })
-            response.end("<h1>Página não encontrada</h1>")
+            if (callback) {
+                if (!callback(req, res)) {
+                    res.writeHead(404, { "Content-Type": "text/html; charset=utf8" })
+                    res.end("<h1>Página não encontrada</h1>")
+                };
+            }
+
         } else {
-            response.writeHead(200, { "Content-Type": "text/html" })
-            response.write(data);
-            response.end();
+            res.writeHead(200, { "Content-Type": "text/html" })
+            res.write(data);
+            res.end();
         }
 
 
     })
+}
+
+
+function handleRequest(req, res) {
+    let path = url.parse(req.url).pathname;
+
+    if (path == "/teste") {
+        res.end("teste")
+        return true;
+    }
+
+    return false;
+}
+
+
+http.createServer((request, response) => {
+
+    handleFile(request, response, handleRequest);
+
 
 }).listen(3000, erro => {
     if (erro) {
